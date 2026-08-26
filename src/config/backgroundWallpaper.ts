0d733aadd3e4c354359b853eddfc90458dff9a35
JunRutey <JunRutey@users.noreply.github.com>
2026-08-27 01:31:06 +0800

    feat: 更换壁纸为自定义图片

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -37,24 +37,26 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	 */
 	src: {
 		// 桌面背景图片（支持单张或多张随机）
-		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/CustomWallpaper/116885360_p0.jpg",
+			"assets/images/CustomWallpaper/126757682_p0.png",
+			"assets/images/CustomWallpaper/137513259_p0.jpg",
+			"assets/images/CustomWallpaper/139379071_p0.jpg",
+			"assets/images/CustomWallpaper/140161372_p0.jpg",
+			"assets/images/CustomWallpaper/141050669_p0.jpg",
+			"assets/images/CustomWallpaper/4222454_bg.jpg",
+			"assets/images/CustomWallpaper/IMG_20260827_011649.png",
 		],
 		// 移动背景图片（支持单张或多张随机）
-		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/CustomWallpaper/116885360_p0.jpg",
+			"assets/images/CustomWallpaper/126757682_p0.png",
+			"assets/images/CustomWallpaper/137513259_p0.jpg",
+			"assets/images/CustomWallpaper/139379071_p0.jpg",
+			"assets/images/CustomWallpaper/140161372_p0.jpg",
+			"assets/images/CustomWallpaper/141050669_p0.jpg",
+			"assets/images/CustomWallpaper/4222454_bg.jpg",
+			"assets/images/CustomWallpaper/IMG_20260827_011649.png",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
