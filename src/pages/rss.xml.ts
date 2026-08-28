9a89d70c60c55339db14e72349187c1f5981293b
JunRutey <JunRutey@users.noreply.github.com>
2026-08-28 16:53:26 +0800

    fix: RSS源添加xmlns:atom命名空间声明

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,35 +1,46 @@
-import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { siteConfig } from "@/config";
 import { getSortedPosts } from "@/utils/content-utils";
 import { formatDateI18nWithTime } from "@/utils/date-utils";
 import { url } from "@/utils/url-utils";
-import i18nKey from "../i18n/i18nKey";
-import { i18n } from "../i18n/translation";
-import pkg from "../../package.json";
 
 export async function GET(context: APIContext) {
 	const posts = await getSortedPosts();
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: siteConfig.site_url,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.published,
-			description: post.data.description || "",
-			link: url(`${post.id}/`),
-		})),
-		customData: `<language>zh-cn</language>
+
+	const items = posts
+		.map(
+			(post) => `
+	<item>
+		<title><![CDATA[${post.data.title}]]></title>
+		<link>${siteConfig.site_url}${url(`${post.id}/`)}</link>
+		<guid isPermaLink="true">${siteConfig.site_url}${url(`${post.id}/`)}</guid>
+		<pubDate>${new Date(post.data.published).toUTCString()}</pubDate>
+		<description><![CDATA[${post.data.description || ""}]]></description>
+	</item>`,
+		)
+		.join("");
+
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>${siteConfig.title}</title>
+		<description>${siteConfig.description}</description>
+		<link>${siteConfig.site_url}/</link>
+		<language>zh-cn</language>
 		<atom:link href="${siteConfig.site_url}/rss.xml" rel="self" type="application/rss+xml" />
 		<image>
 			<url>${siteConfig.site_url}/favicon/firefly-32.png</url>
 			<title>${siteConfig.title}</title>
 			<link>${siteConfig.site_url}</link>
 		</image>
-		<generator>${i18n(i18nKey.rssWhatIsRSS)}</generator>
-		<templateThemeVersion>${pkg.version}</templateThemeVersion>
-		<templateThemeUrl>https://github.com/JunRutey/Firefly</templateThemeUrl>
-		<lastBuildDate>${formatDateI18nWithTime(new Date())}</lastBuildDate>`,
+		<lastBuildDate>${formatDateI18nWithTime(new Date())}</lastBuildDate>
+		${items}
+	</channel>
+</rss>`;
+
+	return new Response(xml, {
+		headers: {
+			"Content-Type": "application/rss+xml; charset=utf-8",
+		},
 	});
 }
